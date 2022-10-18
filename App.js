702630c04536e9136f7e175aca3e5ff8dd1f6d8a
samueljbp <samuel_jbp@yahoo.com.br>
2022-10-18 11:22:11 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {StateProvider} from './src/contexts/StateContext';
+
+export default () => {
+    return (
+        <StateProvider>
+            <View>
+                <Text>Hello man</Text>
+            </View>
+        </StateProvider>
+    );
+};
